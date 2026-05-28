3eb1d72395bcef8d42f95b52548beb4e4ea73cc6
takeC00 <take.614@icloud.com>
2026-05-28 23:25:22 +0900

    first commit

---
+++ RallyMatch/RallyMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyMatchApp.swift
+//  RallyMatch
+//
+//  Created by 服部剛士 on 2026/05/28.
+//
+
+import SwiftUI
+
+@main
+struct RallyMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
